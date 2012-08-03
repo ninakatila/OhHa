542478193dfa49bc7e252418f2194e9a57f23b33
Nina Katila <nkatila@gmail.com>
2012-08-03 22:59:23 +0300

    luotu projekti netbeansiin

---
+++ Jasenkortisto2/src/jasenkortisto2/Jasenkortisto2.java
@@ -0,0 +1,9 @@
+package jasenkortisto2;
+
+// Nina Katila Viikko # Tehtävä #.#
+public class Jasenkortisto2 {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
